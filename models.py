da8110665588e2e91387e7370f61a6a3f0a74bcb
Patrik Karlström <jp.karlstrom@gmail.com>
2017-12-08 00:05:44 +0100

    Autoload models

--- models.py
+++ models.py
@@ -1,22 +1,25 @@
-from sqlalchemy.orm import relationship, backref
 from database import Base
 
-print(Base.metadata.tables)
-
 class Users(Base):
-    __tablename__ = Base.metadata.tables['users']
+    __tablename__ = 'users'
+    __table_args__ = {'autoload':True}
 
 class Games(Base):
-    __tablename__ = Base.metadata.tables['games']
+    __tablename__ = 'games'
+    __table_args__ = {'autoload':True}
 
 class Missions(Base):
-    __tablename__ = Base.metadata.tables['missions']
+    __tablename__ = 'missions'
+    __table_args__ = {'autoload':True}
 
 class Turns(Base):
-    __tablename__ = Base.metadata.tables['turns']
+    __tablename__ = 'turns'
+    __table_args__ = {'autoload':True}
 
 class Nominees(Base):
-    __tablename__ = Base.metadata.tables['nominees']
+    __tablename__ = 'nominees'
+    __table_args__ = {'autoload':True}
 
 class Votes(Base):
-    __tablename__ = Base.metadata.tables['votes']
+    __tablename__ = 'votes'
+    __table_args__ = {'autoload':True}
