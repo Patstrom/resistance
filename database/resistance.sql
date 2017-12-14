b30860f2d8f1378610ca00081973fc0075460972
Patrik Karlström <jp.karlstrom@gmail.com>
2017-12-14 17:17:11 +0100

    Add distinction between votes for nominees and for success of mission

--- database/resistance.sql
+++ database/resistance.sql
@@ -23,27 +23,36 @@ CREATE TABLE missions(
     id              serial primary key,
     game_id         integer not null references Games,
     fails_required  smallint check(fails_required between 1 and 2),
-    people_required smallint check(people_required between 2 and 5)
+    people_required smallint check(people_required between 2 and 5),
+    success boolean default false
 );
 
 CREATE TABLE turns(
     id          serial primary key,
     mission_id  integer not null references Missions,
     leader      integer not null references Players,
+    approved    boolean default false
 );
 
 CREATE TABLE nominees(
     turn_id     integer references Turns,
     player_id   integer references Players,
-    primary key(turn_id, player_id)
+    primary key (turn_id, player_id)
 );
 
-CREATE TABLE votes (
+CREATE TABLE turn_votes (
     turn_id     integer references Turns,
     player_id   integer references Players,
     approve     boolean not null,
     primary key (turn_id, player_id)
 );
+
+CREATE TABLE mission_votes(
+    mission_id  integer references Missions
+    player_id   integer references Players,
+    fail        boolean default false,
+    primary key (mission_id, player_id)
+)
 
 CREATE TABLE posts(
     id          serial primary key,
