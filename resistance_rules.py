ef95524429a77ebf68a418efd583d3a2679b60fb
Patrik Karlström <jp.karlstrom@gmail.com>
2018-01-03 18:11:03 +0100

    Add comments to rules

--- resistance_rules.py
+++ resistance_rules.py
@@ -1,3 +1,4 @@
+# Maps the number of players to number of spies
 number_of_spies = {
     5: 2,
     6: 2,
@@ -7,6 +8,7 @@ number_of_spies = {
     10: 4
 }
 
+# Maps the number of players to how many must be nominated for each mission.
 number_of_players_for_mission = {
     5: [2, 3, 2, 3, 3],
     6: [2, 3, 4, 3, 4],
